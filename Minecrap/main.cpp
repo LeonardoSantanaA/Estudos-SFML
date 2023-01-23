432a9cf28ebc8523df3fdc45c0ec6ce069b9c840
lekaum <lkdev496@gmail.com>
2023-01-23 16:34:56 -0300

    random position for block

--- Minecrap/main.cpp
+++ Minecrap/main.cpp
@@ -1,9 +1,11 @@
 #include <SFML/Graphics.hpp>
 #include <memory.h>
+#include <iostream>
+#include <experimental/random>
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(1280, 720), "Minecrap");
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Minecrap");
     /*
     //criacao do retangulo, posso criar usando um ponteiro, para utilizar ponteiro eu preciso passar um valor para armazenar na heap
     //sempre que criar um ponteiro, lembre-se de destrui-lo!
@@ -28,7 +30,10 @@ int main()
     sf::RectangleShape object;
     object.setSize(sf::Vector2f(100.f, 100.f));
     object.setFillColor(sf::Color(0, 255, 0));
-    object.setPosition(sf::Vector2f(10.f, 10.f));
+    
+    //gerando valor randomico
+    float x = static_cast<float>( std::experimental::randint(10, int(window.getSize().x - object.getSize().x) ) );
+    object.setPosition(x, 10.f);
 
     while (window.isOpen())
     {
@@ -39,8 +44,13 @@ int main()
                 window.close();
         }
 
-       // object.move(0.f, 0.9f); //fazer o objeto sempre movimentar pra baixo
-        
+        object.move(0.f, 0.02f); //fazer o objeto sempre movimentar pra baixo
+        if(object.getPosition().y > window.getSize().y) //verifica se saiu da tela por baixo
+        {
+          x = static_cast<float>( std::experimental::randint(10, int(window.getSize().x - object.getSize().x) ) ); //vou gerar o numero randomico novamente
+          object.setPosition(x, 10.f);
+        }       
+
         window.clear();
         window.draw(object);
         window.display();
