eef815b7a01cec725f6f1da1366b95d33c254e28
lekaum <lkdev496@gmail.com>
2023-01-24 14:32:38 -0300

    Gameover added

--- Minecrap/main.cpp
+++ Minecrap/main.cpp
@@ -1,6 +1,7 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Graphics/Rect.hpp>
 #include <SFML/Graphics/Sprite.hpp>
+#include <SFML/Window/Keyboard.hpp>
 #include <SFML/Window/WindowStyle.hpp>
 #include <cstdlib>
 #include <memory.h>
@@ -15,7 +16,7 @@ int main()
     window.setPosition(sf::Vector2i(30, 30)); //posicao inicial da janela
     window.setMouseCursorVisible(false); //deixa o mouse invisivel
     
-    bool canPress = true;
+    bool gameover = false;
     bool pressed = false; //verifica se o jogador esta com o botao do mouse pressionado
     float velocity = 2.f;
     /*
@@ -47,14 +48,32 @@ int main()
     */  
 
     //Font
-    sf::Font font;
+    sf::Font font, jet;
     if(!font.loadFromFile("./Minecraft.ttf"))
     {
       std::cerr << "Erro ao carregar a font! Consulte a integridade dos seus arquivos." << std::endl;
       return EXIT_FAILURE;
     }
-    sf::Text score, life; 
+    if(!jet.loadFromFile("./jetbrains.ttf"))
+    {
+      std::cerr << "Erro ao carregar a font! Consulte a integridade dos seus arquivos." << std::endl;
+      return EXIT_FAILURE;
+    }
+    sf::Text score, life, gameover_text, restart;
 
+    //restart
+    restart.setFont(jet);
+    restart.setFillColor(sf::Color::Yellow);
+    restart.setPosition(320, 310);
+    restart.setString("Aperte R para tentar novamente");
+
+    //gameover
+    gameover_text.setFont(font);
+    gameover_text.setFillColor(sf::Color::White);
+    gameover_text.setString("GAME OVER"); 
+    gameover_text.setPosition(290, 200);
+    gameover_text.setCharacterSize(100);
+        
     //score
     int points = 0;
     score.setFont(font);
@@ -108,105 +127,129 @@ int main()
 
     while (window.isOpen())
     {
-        sf::Event event;
-        while (window.pollEvent(event))
+      sf::Event event;
+      while (window.pollEvent(event))
+      {
+        if (event.type == sf::Event::Closed)
+          window.close();
+
+        //tratamento da janela
+        if(event.type == sf::Event::Resized)  //evento que deteca se o usuario redimensionou a janela
         {
-            if (event.type == sf::Event::Closed)
-                window.close();
-
-            //tratamento da janela
-            if(event.type == sf::Event::Resized)  //evento que deteca se o usuario redimensionou a janela
-            {
-              sf::FloatRect visible_area(0, 0, event.size.width, event.size.height); //cria um novo retangulo, que vai ser nossa area visivel, o tamanho é com base no tanto que o usuario redimensionou
-              window.setView(sf::View(visible_area)); //setar a visibilidade nova
-            }
-            
-            //tratamento da jogabilidade
-            if(event.type == event.MouseButtonPressed)
-            {
-              pressed = false;
-            }
-
-            pos_mouse_win = sf::Mouse::getPosition(window); //ficar sempre pegando a posicao do meu mouse
-            pos_mouse_coord = window.mapPixelToCoords(pos_mouse_win); //converte a posicao do mouse em relacao a janela
-            mouse.setPosition((sf::Vector2f)pos_mouse_win); //a posicao da minha picareta (mouse) 
+          sf::FloatRect visible_area(0, 0, event.size.width, event.size.height); //cria um novo retangulo, que vai ser nossa area visivel, o tamanho é com base no tanto que o usuario redimensionou
+          window.setView(sf::View(visible_area)); //setar a visibilidade nova
         }
-
-        if(objs.size() < max_objs) //adiciona os objetos no vetor
+         
+        //tratamento da jogabilidade
+        if(event.type == event.MouseButtonPressed)
         {
-          if(obj_vel >= obj_vel_max)
-          {
-            x = static_cast<float>( std::experimental::randint(10, int(window.getSize().x - texture.getSize().x) ) ); //vou gerar o numero randomico novamente
-            object.setPosition(x, 10.f);
-            objs.push_back(object);
-            obj_vel = 0.f;
-          }else
-          {
-            obj_vel += 1.f;
-          }
+        pressed = false;
         }
+        pos_mouse_win = sf::Mouse::getPosition(window); //ficar sempre pegando a posicao do meu mouse
+        pos_mouse_coord = window.mapPixelToCoords(pos_mouse_win); //converte a posicao do mouse em relacao a janela
+        mouse.setPosition((sf::Vector2f)pos_mouse_win); //a posicao da minha picareta (mouse) 
+      }
 
-        //mover e deletar os objetos do vetor
-        for (size_t i{}; i < objs.size(); ++i)
-        {
-          bool del = false; //controla se vai ou nao deletar o obj
-          objs[i].move(0.f, velocity); //fazer o objeto sempre movimentar pra baixo
-
-          if(sf::Mouse::isButtonPressed(sf::Mouse::Left) && !pressed) //verifica se clicou com o botao esquerdo
-          {
-            if(objs[i].getGlobalBounds().contains(pos_mouse_coord)) //essas funcoes verifica se na coordernada que eu cliquei com o botao do mouse, continha algum pixel do retangulo do objeto, 
-                                                                 //fazendo assim uma verificacao de colisao
-            {
-              pressed = true;
-              del = true;
-              points += 10;
-              score.setString("Score: " + std::to_string(points));
-              if(points % 100 == 0)
-              {
-                velocity += 0.1f;
-                std::cout << "Nova velocidade: " << velocity << std::endl;
-              }
-              if(points % 1000 == 0)
-              {
-                ++health;
-                std::cout << "Voce ganhou uma vida: " << health << std::endl;
-                life.setString("Life: " + std::to_string(health));
-              }
-            }
-          }
-
-          if(objs[i].getPosition().y > window.getSize().y) //verifica se saiu da tela por baixo
-          {
-            --health;
-            life.setString("Life: " + std::to_string(health));
-            del = true;
-            //gameover
-            if(health <= 0)
-            {
-              window.close();
-            }
-          }  
-
-          if(del)
-          {
-            objs.erase(objs.begin() + i); //o erase() recebe um iterator, por isso eu tenho que pegar o primeiro obj pelo begin() que retorna um iterator, e somo i para ir pra posicao do obj que quero deletar
-          }
-
-        }
-
-        window.clear();
-        window.draw(background);
+      if(gameover)
+      {
+        window.setMouseCursorVisible(true);
+        window.clear(sf::Color::Black);
         window.draw(score);
-        window.draw(life);
-        //foreach que percorre os objs para desenhar
-        for (auto &e : objs)
-        {
-          window.draw(e);
-        }
-        window.draw(mouse);
+        window.draw(restart);
+        window.draw(gameover_text);
         window.display();
-    }
-   
 
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::R))
+        {
+          objs.clear();
+          health = 5;
+          life.setString("Life: " + std::to_string(health)); 
+          points = 0;
+          score.setString("Score: " + std::to_string(points));
+          window.setMouseCursorVisible(false);
+          velocity = 2.f;
+          gameover = false;
+        }
+
+      }else 
+      { 
+      //Codigo do game  
+      if(objs.size() < max_objs) //adiciona os objetos no vetor
+      {
+        if(obj_vel >= obj_vel_max)
+        {
+          x = static_cast<float>( std::experimental::randint(10, int(window.getSize().x - texture.getSize().x) ) ); //vou gerar o numero randomico novamente
+          object.setPosition(x, 10.f);
+          objs.push_back(object);
+          obj_vel = 0.f;
+        }else
+        {
+          obj_vel += 1.f;
+        }
+      }
+
+      //mover e deletar os objetos do vetor
+      for (size_t i{}; i < objs.size(); ++i)
+      {
+        bool del = false; //controla se vai ou nao deletar o obj
+        objs[i].move(0.f, velocity); //fazer o objeto sempre movimentar pra baixo
+
+        if(sf::Mouse::isButtonPressed(sf::Mouse::Left) && !pressed) //verifica se clicou com o botao esquerdo
+        {
+          if(objs[i].getGlobalBounds().contains(pos_mouse_coord)) //essas funcoes verifica se na coordernada que eu cliquei com o botao do mouse, continha algum pixel do retangulo do objeto, 
+                                                                 //fazendo assim uma verificacao de colisao
+          {
+            pressed = true;
+            del = true;
+            points += 10;
+            score.setString("Score: " + std::to_string(points));
+            if(points % 100 == 0)
+            {
+              velocity += 0.1f; 
+              std::cout << "Nova velocidade: " << velocity << std::endl;
+            }
+            if(points % 1000 == 0)
+            {
+              ++health;
+              std::cout << "Voce ganhou uma vida: " << health << std::endl;
+              life.setString("Life: " + std::to_string(health));
+            }
+          }
+        }
+
+        if(objs[i].getPosition().y > window.getSize().y) //verifica se saiu da tela por baixo
+        {
+          --health;
+          life.setString("Life: " + std::to_string(health));
+          del = true;
+          //gameover
+          if(health <= 0)
+          {
+            gameover = true;
+          }
+        }  
+
+        if(del)
+        {
+          objs.erase(objs.begin() + i); //o erase() recebe um iterator, por isso eu tenho que pegar o primeiro obj pelo begin() que retorna um iterator, e somo i para ir pra posicao do obj que quero deletar
+        }
+
+      }
+
+      window.clear();
+      window.draw(background);
+      window.draw(score);
+      window.draw(life);
+      //foreach que percorre os objs para desenhar
+      for (auto &e : objs)
+      {
+        window.draw(e);
+      }
+      window.draw(mouse);
+      window.display();
+      }
+
+    } //While loop game principal
+   
     return EXIT_SUCCESS;
 }
